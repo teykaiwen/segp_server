a31094eca9a2bdea6d1c2f9e0c57370e5c4f7f72
teykaiwen <tey.kaiwen@gmail.com>
2019-04-17 12:09:25 +0800

    add 2.5 to formula

--- server.py
+++ server.py
@@ -146,7 +146,7 @@ def upload():
     band1_median = band1_median[0]
     # calculate ssc value
     # formula given by client
-    ssc_value = -0.0368 * band1_median + 5.2825
+    ssc_value = -0.0368 * band1_median + 5.2825 +2.5
     ssc_value = round(ssc_value, 6)
     df['ssc'] = ssc_value
     df.at[1, 'ssc'] = None
@@ -197,7 +197,10 @@ def upload():
     if ssc_value >= -0.5 and ssc_value < 0:
         category = 'Clean'
     
-    elif ssc_value >= 0:
+    elif ssc_value >= 0 and ssc_value < 0.5:
+        category = 'Average'
+
+    elif ssc_value >= 0.5:
         category = 'Dirty'
 
     else:
